231b7d85760d57e6a4e2c76d5abe14eafd0d2655
salomejarnouen <salomejarnouen@gmail.com>
2019-10-08 15:25:16 +0200

    Création des models User et Address

---
+++ src/main/java/models/User.java
@@ -0,0 +1,26 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String photoUrl;
+    private String bio;
+    private Address address;
+    private List<String> friendsId;
+
+
+
+
+}
