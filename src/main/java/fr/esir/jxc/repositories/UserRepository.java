2d2ca344c5867d12e5d9297ef47aeceba2904104
salomejarnouen <salomejarnouen@gmail.com>
2019-10-15 18:17:57 +0200

    configuration elasticsearch

--- src/main/java/fr/esir/jxc/repositories/UserRepository.java
+++ src/main/java/fr/esir/jxc/repositories/UserRepository.java
@@ -3,9 +3,9 @@ package fr.esir.jxc.repositories;
 import fr.esir.jxc.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends ElasticsearchRepository<User, String> {
-
     Page<User> findAll();
-    
 }
