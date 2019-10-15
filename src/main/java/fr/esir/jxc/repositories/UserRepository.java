70c18336b24ea1e2dfeb787e479404aa80e9fafe
salomejarnouen <salomejarnouen@gmail.com>
2019-10-15 16:49:49 +0200

    création du UserRepository

---
+++ src/main/java/fr/esir/jxc/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package fr.esir.jxc.repositories;
+
+import fr.esir.jxc.models.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface UserRepository extends ElasticsearchRepository<User, String> {
+}
