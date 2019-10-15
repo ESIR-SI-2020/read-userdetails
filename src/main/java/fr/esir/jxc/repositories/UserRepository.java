494f91b79ed6c14434190e1e600726c325053d1a
salomejarnouen <salomejarnouen@gmail.com>
2019-10-15 16:52:11 +0200

    findAll dans UserRepository

--- src/main/java/fr/esir/jxc/repositories/UserRepository.java
+++ src/main/java/fr/esir/jxc/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package fr.esir.jxc.repositories;
 
 import fr.esir.jxc.models.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface UserRepository extends ElasticsearchRepository<User, String> {
+
+    Page<User> findAll();
+    
 }
