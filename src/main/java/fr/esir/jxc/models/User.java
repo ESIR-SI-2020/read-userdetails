fa01cca0038ac8a256e6e211e329ba1182d97ac4
oumar <balloumar49@hotmail.com>
2019-10-19 20:46:23 +0200

    work in progress

--- src/main/java/fr/esir/jxc/models/User.java
+++ src/main/java/fr/esir/jxc/models/User.java
@@ -3,7 +3,10 @@ package fr.esir.jxc.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.List;
 
@@ -13,12 +16,15 @@ import java.util.List;
 @Document(indexName = "pocket", type="user")
 public class User {
 
+    @Id
     private String id;
     private String username;
     private String password;
     private String email;
     private String photoUrl;
     private String bio;
+
+    @Field(type = FieldType.Object)
     private Address address;
     private List<String> friendsId;
 
